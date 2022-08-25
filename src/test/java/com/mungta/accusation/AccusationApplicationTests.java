321476021d97ff7ae29b8c4be108e3e0b6e1e595
rlapwl <khji1210@naver.com>
2022-08-26 01:26:28 +0900

    test pass

--- src/test/java/com/mungta/accusation/AccusationApplicationTests.java
+++ src/test/java/com/mungta/accusation/AccusationApplicationTests.java
@@ -3,7 +3,7 @@ package com.mungta.accusation;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class AccusationApplicationTests {
 
 	@Test
